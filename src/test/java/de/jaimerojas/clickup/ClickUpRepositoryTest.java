ced337bb68c736a4dc16b761206e256ccbef8292
jaim3r0jas <148350284+jaim3r0jas@users.noreply.github.com>
2025-04-04 11:04:56 +0200

    Feature/add tests coverage to gh workflow (#23)
    * ad-hoc: major workflows refactor
    
    * ad-hoc: major workflows refactor #2
    
    * ad-hoc: major workflows refactor #3
    
    Feature/add tests coverage to gh workflow (#24)
    
    * ad-hoc: major workflows refactor
    
    * ad-hoc: major workflows refactor #3
    
    * ad-hoc: major workflows refactor #4
    
    * ad-hoc: major workflows refactor #5
    
    * ad-hoc: major workflows refactor #6
    
    * ad-hoc: major workflows refactor #7
    
    * ad-hoc: major workflows refactor #8
    
    * ad-hoc: major workflows refactor #9
    
    * ad-hoc: major workflows refactor #9
    
    * ad-hoc: major workflows refactor #10
    
    * ad-hoc: major workflows refactor #11
    
    * ad-hoc: major workflows refactor #12
    
    * ad-hoc: major workflows refactor #13
    
    * ad-hoc: major workflows refactor #14
    
    * ad-hoc: major workflows refactor #15
    
    * ad-hoc: major workflows refactor #16
    
    * ad-hoc: major workflows refactor #17
    
    * ad-hoc: major workflows refactor #19

---
+++ src/test/java/de/jaimerojas/clickup/ClickUpRepositoryTest.java
@@ -0,0 +1,58 @@
+package de.jaimerojas.clickup;
+
+import org.junit.jupiter.api.Test;
+
+class ClickUpRepositoryTest {
+
+    @Test
+    void findTask() {
+    }
+
+    @Test
+    void getIssues() {
+    }
+
+    @Test
+    void testGetIssues() {
+    }
+
+    @Test
+    void updateTimeSpent() {
+    }
+
+    @Test
+    void setTaskState() {
+    }
+
+    @Test
+    void getAvailableTaskStates() {
+    }
+
+    @Test
+    void getFeatures() {
+    }
+
+    @Test
+    void createCancellableConnection() {
+    }
+
+    @Test
+    void fetchWorkspaces() {
+    }
+
+    @Test
+    void fetchSpaces() {
+    }
+
+    @Test
+    void fetchLists() {
+    }
+
+    @Test
+    void clearSelectedWorkspace() {
+    }
+
+    @Test
+    void clearSelectedSpace() {
+    }
+}
